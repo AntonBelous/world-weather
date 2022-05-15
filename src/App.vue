f26b20a42c950f3e8ae5e4f61d76dd1d184c3018
AntonBelous <woodgate@i.ua>
2022-05-15 10:42:41 +0300

    project : error handler for getCurrentPosition function

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,10 @@
 <template>
-  <span
-      class="geolocation-alert"
-      v-if="!isAppReady && !isGeoEnabled"
-  >
-    Please grant this site access to your geolocation
-  </span>
   <transition>
-    <div class="main container" v-if="isAppReady && isGeoEnabled">
+    <div class="main container" v-if="isAppReady">
       <h2 class="main__title">
         World Weather
       </h2>
-      <template v-if="currentCity.id">
+      <template v-if="currentCity.id && isGeoEnabled">
         <h4 class="main__subtitle" >
           Watch weather in your current location
         </h4>
@@ -23,6 +17,9 @@
           />
         </div>
       </template>
+      <h4 class="main__subtitle" v-else>
+        Please grant this site access to your geolocation
+      </h4>
       <div class="main__body-row" v-if="cities.length">
         <transition-group>
           <div
@@ -135,36 +132,35 @@ export default {
       localStorage.setItem('cities', JSON.stringify(filteredCities))
     }
 
-    // This is a function that is called when the user grants the site access to their geolocation. It will get the
-    // weather data for the user's current location and then set the currentCity object to the data that it gets back.
-    navigator.geolocation.getCurrentPosition(async (data) => {
-      try {
-        const isGeoPosition = await navigator.permissions.query({ name: 'geolocation' })
-        isGeoEnabled.value = isGeoPosition.state === 'granted'
-        if (!isGeoEnabled.value) return
-      } catch (e) {
-        throw new Error(e)
-      }
+    // This is a function that is called when the user clicks the add button on the modal. It will get the weather data
+    // for the city that the user entered and then add it to the cities array.
+    navigator.geolocation.getCurrentPosition(
+      async (data) => {
+        try {
+          const weather = await getWeatherData([data.coords.latitude, data.coords.longitude])
+          const cityStorage = JSON.parse(localStorage.getItem('currentCity'))
+          const formatData = { ...weather }
 
-      try {
-        const weather = await getWeatherData([data.coords.latitude, data.coords.longitude])
-        const cityStorage = JSON.parse(localStorage.getItem('currentCity'))
-        const formatData = { ...weather }
+          formatData.time = moment().format()
 
-        formatData.time = moment().format()
+          if (cityStorage && cityStorage.id === formatData.id) {
+            currentCity.value = cityStorage
+          } else {
+            currentCity.value = formatData
+            localStorage.setItem('currentCity', JSON.stringify(formatData))
+          }
 
-        if (cityStorage && cityStorage.id === formatData.id) {
-          currentCity.value = cityStorage
-        } else {
-          currentCity.value = formatData
-          localStorage.setItem('currentCity', JSON.stringify(formatData))
+          isGeoEnabled.value = true
+          isAppReady.value = true
+        } catch (e) {
+          throw new Error(e)
         }
-
+      },
+      (e) => {
+        if (e.PERMISSION_DENIED) isGeoEnabled.value = false
         isAppReady.value = true
-      } catch (e) {
-        throw new Error(e)
       }
-    })
+    )
 
     // Checking if there is a localStorage item called cities. If there is, it will set the cities array to the value of
     // the localStorage item.
